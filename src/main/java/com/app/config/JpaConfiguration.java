e8e59c5c01fa7692744c0aa9b0895119d568571f
nellyiriberri <nellyiriberri@gmail.com>
2020-06-09 13:47:14 +0200

    Conf fix

--- src/main/java/com/app/config/JpaConfiguration.java
+++ src/main/java/com/app/config/JpaConfiguration.java
@@ -2,14 +2,18 @@ package com.app.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import java.util.Properties;
 
+@Configuration
+@EnableTransactionManagement
 public class JpaConfiguration {
 
     private Properties hibernateProperties(){
